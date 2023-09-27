0c55646b77466b255572ac5edf0dcddd91e9e4d0
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 17:08:48 +0100

    Add files via upload
    Updated

---
+++ Continue1/src/continue1/Continue1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package continue1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class Continue1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 0; i <=3; i++) 
+{ 
+System.out.println("Iteration number:" + i); if(i == 2) break; 
+} 
+
+    }
+    
+}
